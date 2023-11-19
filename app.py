c75b48001a287f7ef058cbca3183ab9dd901e690
Kiran <kiran.kp2004@gmail.com>
2023-11-19 19:41:59 +0530

    mouse tracking done

--- app.py
+++ app.py
@@ -1,30 +1,63 @@
+# app.py
+
 from flask import Flask, render_template, request, redirect, url_for
+import csv
+import json
 
 app = Flask(__name__)
 
-# Survey question
-question = "Do you enjoy using Flask?"
+questions = [
+    {'id': 1, 'question': 'Do you like Flask?'},
+    # Add more questions as needed
+]
 
-# Survey responses
 responses = []
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html", question=question)
+    question = questions[0]['question']  # Display the first question
+    return render_template('index.html', question=question)
 
 
-@app.route("/submit", methods=["POST"])
+@app.route('/submit', methods=['POST'])
 def submit():
-    response = request.form.get("response")
+    response = request.form['response']
     responses.append(response)
-    return redirect(url_for("index"))
+
+    print(responses)
+    
+    # Get mouse tracking data
+    mouse_data = request.form['mouse_data']
+
+    print('Received Mouse Data:', mouse_data)
+
+    try:
+        mouse_data_list = json.loads(mouse_data)
+    except json.JSONDecodeError:
+        mouse_data_list = []  # Handle the case where JSON decoding fails
+
+    # Add mouse tracking data to CSV file
+    write_mouse_tracking_to_csv(response, mouse_data_list)
+
+    # Move to the next question or show results when all questions are answered
+    next_question_index = len(responses)
+    if next_question_index < len(questions):
+        question = questions[next_question_index]['question']
+        return render_template('index.html', question=question)
+    else:
+        return redirect(url_for('results'))
 
 
-@app.route("/results")
+def write_mouse_tracking_to_csv(response, mouse_data_list):
+    with open('mouse_tracking.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        for entry in mouse_data_list:
+            writer.writerow([response, entry.get('x', ''), entry.get('y', '')])
+
+@app.route('/results')
 def results():
-    return render_template("results.html", question=question, responses=responses)
+    # Display survey results
+    return render_template('results.html', responses=responses)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
