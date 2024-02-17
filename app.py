2b78afce1f176ebb1d4c306debf9dc8f4ca538c7
Shubham Salunkhe <sssalunkhe_b20@ce.vjti.ac.in>
2024-02-17 12:58:30 +0530

    fixed app.py
    fixed app.py

--- app.py
+++ app.py
@@ -22,48 +22,9 @@ image_name = ""
 responseTimes = []
 currentEmotions = []
 
-# Force Matplotlib to use non-interactive backend
-matplotlib.use('Agg')
-
-def plot_mouse_tracking(label, mouse_data_list):
-    cor_x = [point["x"] for point in mouse_data_list]
-    cor_y = [point["y"] for point in mouse_data_list]
-
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
-
-    folder_path = os.path.join("static", "graphs", label)
-    os.makedirs(folder_path, exist_ok=True)  # Create folder if it doesn't exist
-
-    plt.plot(cor_x, cor_y)
-    plt.title("Mouse Tracking")
-    plt.xlabel("X-coordinate")
-    plt.ylabel("Y-coordinate")
-    plt.xlim([0, 1600])
-    plt.ylim([0, 700])
-    graph_path = os.path.join(folder_path, f"{label}_{timestamp}_graph.png")
-    plt.savefig(graph_path)
-    plt.close()
-
-def write_mouse_tracking_to_csv(label, response, responseTime, currentEmotion, mouse_data_list):
-    # Plotting in a separate thread to avoid Matplotlib warning
-    plot_thread = Thread(target=plot_mouse_tracking, args=(label, mouse_data_list))
-    plot_thread.start()
-
-    cor_x = [point["x"] for point in mouse_data_list]
-    cor_y = [point["y"] for point in mouse_data_list]
-
-    for i in mouse_data_list:
-        cor_x.append(i["x"])
-        cor_y.append(i["y"])
-
-    with open('mouse_tracking.csv', mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([label, response, responseTime, currentEmotion] + cor_x + cor_y)
-
 @app.route('/')
 def index():
-    images = [image for image in os.listdir(os.path.join(app.static_folder, 'images')) if
-              image.endswith(('.png', '.jpg', '.jpeg'))]
+    images = [image for image in os.listdir(os.path.join(app.static_folder, 'images')) if image.endswith(('.png', '.jpg', '.jpeg'))]
     random_image = choice(images) if images else None
     
     image_emotion=random_image[:-10]
@@ -84,9 +45,6 @@ def submit():
 
     label = request.form['label']
     labels.append(label)
-
-    # Get mouse tracking data
-    mouse_data = request.form['mouse_data']
 
     responseTime = request.form['responseTime']
     responseTimes.append(responseTime)
